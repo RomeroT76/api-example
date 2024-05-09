eea229775732c1f713fd6821674b5d3e2a1a87ef
RomeroT76 <robertoart76x@gmail.com>
2024-05-09 11:36:46 -0500

    commit inical

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"prueba-firebase-6cbd7","appId":"1:887132030949:web:5dc6be56f2ac1fb1b4110c","storageBucket":"prueba-firebase-6cbd7.appspot.com","apiKey":"AIzaSyAMsvicK24kY853YEthpmtiagxD3jA07uk","authDomain":"prueba-firebase-6cbd7.firebaseapp.com","messagingSenderId":"887132030949"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
